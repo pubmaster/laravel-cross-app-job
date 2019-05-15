79b5a54d445de7a267e90da1bc119b0ac7e4dfac
Vladimir Potapenko <Vladimir.Potapenko@mediainstinctgroup.ru>
2019-05-15 18:44:29 +0300

    some composer json fixes

--- src/CrossAppJob.php
+++ src/CrossAppJob.php
@@ -2,6 +2,11 @@
 
 namespace CrossAppJob;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
 /**
  * Class CrossAppJob
  */
